b0de9ae79dd4ec4087e257ef0a322b07e29c5426
Cioara Raul <cioararaul08@gmail.com>
2026-03-17 00:11:03 +0200

    for deployment

--- signaltrack-frontend/src/environments/environment.prod.ts
+++ signaltrack-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: '/api',
-  wsUrl: '',
+  apiUrl: 'https://signaltrack-backend-production.up.railway.app',
+  wsUrl: 'https://signaltrack-backend-production.up.railway.app',
 };
